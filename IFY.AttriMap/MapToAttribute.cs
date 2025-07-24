dd6449d20c7a9f444a0334b02dba82e264727a4d
novaxis-iy <iy@novax.is>
2025-07-24 20:52:33 +0100

    First commit - working MapTo

---
+++ IFY.AttriMap/MapToAttribute.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS9113 // Ignore unused parameters (No logic here)
+
+namespace IFY.AttriMap;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+public sealed class MapToAttribute<TTarget>(string targetProperty, string? transformerMethod = null) : Attribute
+    where TTarget : new()
+{
+}
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+public sealed class MapToAttribute(Type targetType, string targetProperty, string? transformerMethod = null) : Attribute
+{
+}
